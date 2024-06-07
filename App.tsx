2cbd3cfc71b0b59e64d7c5fa659c0a9a8aabf378
Pietro Roversi <pietroroversi@libero.it>
2024-06-07 16:52:19 +0200

    step-4

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import HomeScreen from "./src/views/HomeScreen";
+import { OneSignal } from "react-native-onesignal";
+import { ONE_SIGNAL_APP_ID } from "./src/config/constants";
 
 export default function App() {
+  OneSignal.initialize(ONE_SIGNAL_APP_ID);
+  OneSignal.Notifications.requestPermission(true);
+  
   return <HomeScreen />;
 }
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,2 @@
 export const APPLICATION_URL='https://www.evoluzione.agency'
+export const ONE_SIGNAL_APP_ID='d73f3a0d-0b86-4832-b3ce-91ddd3f92e42'
